3ec724f4fbeefc459b058a500b4a7c9951599593
gaetani <deniscgaetani@gmail.com>
2019-01-30 22:47:16 -0200

    todo-lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { StoreModule } from '@ngrx/store';
+import { reducer } from '../store/reducers';
 import { AppComponent } from './app.component';
+import { MaterialModule } from '../shared/material-module';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    StoreModule.forRoot({ reducer }),
+    StoreDevtoolsModule.instrument()
   ],
   providers: [],
   bootstrap: [AppComponent]
